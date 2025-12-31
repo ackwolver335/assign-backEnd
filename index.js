016c3e9f96b421ef740ed3ddd0ce1c5d860a1f13
ackwolver335 <abhaych335@gmail.com>
2025-12-31 11:57:56 +0530

    Final Configuration added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import connectDB from './config/db.js';
 import auth from './routes/auth.js';
 import tasks from './routes/tasks.js';
 
-dotenv.config();
+dotenv.config({ quiet: true });
 
 const app = express();
 app.use(express.json());
@@ -17,6 +17,11 @@ await connectDB();
 // Routes
 app.use('/api/auth', auth);
 app.use('/api/tasks', tasks);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 app.get("/", (req, res) => {
   res.send("API is running!");
